3a1ab7a0c0773e3408d3f532d11ea08a50d85dda
neptunehub <g.colangiuli@gmail.com>
2025-05-30 13:48:40 +0200

    fix task.py

--- tasks.py
+++ tasks.py
@@ -29,7 +29,8 @@ from app import (app, redis_conn, get_db, save_task_status, get_task_info_from_d
 from config import TEMP_DIR, MAX_DISTANCE, MAX_SONGS_PER_CLUSTER, MAX_SONGS_PER_ARTIST, \
     GMM_COVARIANCE_TYPE, MOOD_LABELS, EMBEDDING_MODEL_PATH, PREDICTION_MODEL_PATH, \
     JELLYFIN_URL, JELLYFIN_USER_ID, JELLYFIN_TOKEN # For create_or_update_playlists_on_jellyfin
-from rq.exceptions import NoSuchJobError
+from rq.job import Job # Import Job class
+from rq.exceptions import NoSuchJobError, InvalidJobOperation
 
 # --- Task-specific Helper Functions ---
 
